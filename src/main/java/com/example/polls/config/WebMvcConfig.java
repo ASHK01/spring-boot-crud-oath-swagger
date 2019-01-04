bd1b4efe4c7d566d397a6198b2b9f8e0fc48d2f4
Aamir <aamir1.sheikh1@gmail.com>
2019-01-04 16:01:15 +0530

    Removing the extra ui code related to polls

--- src/main/java/com/example/polls/config/WebMvcConfig.java
+++ src/main/java/com/example/polls/config/WebMvcConfig.java
@@ -22,5 +22,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .maxAge(MAX_AGE_SECS);
     }
     
+    @Override 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
     
 }
